45322744723b846ac14c072f31f2a0b5f53ab503
robotenique <julianogarcia_1997@hotmail.com>
2018-03-15 11:43:15 -0300

    add commented code for challenge_01

--- parallel-concurrent/challenges/challenge_01.c
+++ parallel-concurrent/challenges/challenge_01.c
@@ -2,25 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-// TODO: Comment and organize
 #define N 80000000
 
-int main(int argc, char const *argv[]) {
-    srand(time(NULL));
-
-    int *v = malloc(N*sizeof(int));
-    for (int i = 0; i < N; i++)
-        v[i] = i + 1;
+// Counts which elements of v are greater than N/2 and which aren't
+void checkNumbers(int *v){
     int k;
     int a = 0, b = 0;
-
-    for (int i = 0; i < (int)(N > 100 ? 50 : 0.2*N); i++)
-        printf("%d, ", v[i]);
-    printf("\n\n");
-    // Time of the first method
-    clock_t t;
-    t = clock();
-
     k = 0;
     while (k < N) {
         if(v[k] > N/2)
@@ -29,12 +16,28 @@ int main(int argc, char const *argv[]) {
             b++;
         k++;
     }
+}
 
+int main(int argc, char const *argv[]) {
+    srand(time(NULL));
+
+    int *v = malloc(N*sizeof(int));
+    for (int i = 0; i < N; i++)
+        v[i] = i + 1;
+
+    printf("50 first numbers: \n");
+    for (int i = 0; i < (int)(N > 100 ? 50 : 0.2*N); i++)
+        printf("%d, ", v[i]);
+
+    // Time of the first method
+    clock_t t;
+    t = clock();
+    checkNumbers(v);
     t = clock() - t;
     double time_taken = 100*((double)t)/CLOCKS_PER_SEC;
-    printf(" time = %fms\n", time_taken);
+    printf("\ntime of the first = %fms\n\n", time_taken);
 
-
+    // Fisher-yates shuffle
     int temp;
     for (int i = 0; i < N; i++) {
         int r = (int) rand()%(i + 1);
@@ -43,26 +46,17 @@ int main(int argc, char const *argv[]) {
         v[i] = temp;
     }
 
+    printf("50 first numbers: \n");
     for (int i = 0; i < (int)(N > 100 ? 50 : 0.2*N); i++)
         printf("%d, ", v[i]);
-    printf("\n\n");
+
 
     // Time of of the second method
     t = clock();
-    k = 0;
-    while (k < N) {
-        if(v[k] > N/2)
-            a++;
-        else
-            b++;
-        k++;
-    }
+    checkNumbers(v);
     t = clock() - t;
     time_taken = 100*((double)t)/CLOCKS_PER_SEC;
-    printf(" time = %fms\n", time_taken);
-
-
-
+    printf("\ntime of the second = %fms\n", time_taken);
 
     return 0;
 }
