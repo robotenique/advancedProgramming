bbdbb45c471f474fb52413661fca9a6d05ae2260
robotenique <julianogarcia_1997@hotmail.com>
2018-06-06 16:23:15 -0300

    add QR and Cholesky factorization

---
+++ numeric-methods/cholesky.py
@@ -0,0 +1,28 @@
+"""Implements Cholesky Matrix Factorization A = LL.T, if A is a positive defined matrix
+"""
+import numpy as np
+
+def main():
+    # Matrix example
+    A = np.array([[1, 1, 3],
+                  [1, 5, 5],
+                  [3, 5, 19]], dtype=float)
+    L = cholesky(A)
+    print(f"L = \n {L}")
+    print(f"LL.T = \n{L@L.T}")
+    print(f"A = \n{A}")
+
+def cholesky(A):
+    L = A.copy()
+    for i in range(len(L)):
+        L[i, i] = np.sqrt(L[i, i])
+        L[i+1:, i] /= L[i, i]
+        L[i, i+1:] *= 0
+        L[i+1:, i+1:] -= np.outer(L[i+1:, i], L[i+1:, i])
+    return L
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ numeric-methods/qr.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def main():
+    A  = np.array(((
+    (3, 64, 4),
+    (4, 0, 4),
+    (24,9, 34)
+    )))
+
+    #A = np.abs(np.floor(np.random.randn(10, 10)*10))
+
+    Q, R = qr(A)
+    print(Q.round(2))
+    print(R.round(2))
+    print((Q@R).round(2))
+
+
+def qr(A):
+    rows, cols = A.shape
+    Q = np.eye(rows)
+    for i in range(cols - (rows == cols)):
+        H = np.eye(rows)
+        H[i:, i:] = get_householder(A[i:, i])
+        Q = np.dot(Q, H)
+        A = np.dot(H, A)
+    return Q, A
+
+def get_householder(a):
+    H = np.eye(a.shape[0])
+    norm = np.linalg.norm
+    if norm(a) == 0 or a == []:
+        return H
+    v = a/(a[0] + np.copysign(norm(a), a[0]))
+    v[0] = 1
+    print(v)
+    H -= (2/np.dot(v, v))*np.dot(v[:, None], v[None, :])
+
+    return H
+
+
+if __name__ == '__main__':
+    main()
